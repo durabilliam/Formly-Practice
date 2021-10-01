9cb74badf2173d1352462a9c9f41889123f8c2b8
William Perry <durabilliam@gmail.com>
2021-10-01 09:19:22 -0230

    add initial form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,109 @@
+import { Placeholder } from '@angular/compiler/src/i18n/i18n_ast';
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+  <div class="container">
+    <h1>{{title}}</h1>
+    <p>This app is to help learn forms with formly</p>
+    <form [formGroup]="form" (ngSubmit)="onSubmit()">
+      <formly-form [form]="form" [fields]="fields" [model]="model">
+      </formly-form>
+      <button type="submit" class="btn btn-default">Submit</button>
+    </form>
+    <pre>{{model | json}}</pre>
+  </div>
+  `,
 })
 export class AppComponent {
-  title = 'AngularFormlyApp';
+  title = "Formly Practice"
+  form = new FormGroup({});
+  model = { email: 'email@gmail.com' };
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'name',
+      type: 'input',
+      templateOptions: {
+        label: 'Name',
+        placeholder: 'Enter name',
+      }
+    },
+    {
+      key: 'email',
+      type: 'input',
+      templateOptions: {
+        type: 'email',
+        label: 'Email',
+        placeholder: 'Enter email',
+      }
+    },
+    {
+      key: 'amount',
+      type: 'input',
+      templateOptions: {
+        type: 'number',
+        label: 'Amount',
+        placeholder: 'Enter Amount'
+      }
+    },
+    {
+      key: 'date_of_birth',
+      type: 'datepicker',
+      templateOptions: {
+        label: 'Date of Birth',
+        placeholder: 'Enter DOB',
+        description: 'Description',
+        required: true,
+      }
+    },
+    {
+      key: 'terms',
+      type: 'checkbox',
+      templateOptions: {
+        label: 'Accept terms',
+        description: 'Please accept out terms of use',
+        required: true
+      }
+    },
+    {
+      key: 'terms2',
+      type: 'toggle',
+      templateOptions: {
+        label: 'Accept terms2',
+        description: 'Please double accept out terms of use',
+        required: true,
+      }
+    },
+    {
+      key: 'description',
+      type: 'textarea',
+      templateOptions: {
+        label: 'Description',
+        placeholder: 'Enter description',
+      }
+    },
+    {
+      key: 'era',
+      type: 'radio',
+      templateOptions: {
+        label: 'Era',
+        placeholder: 'Year of publication',
+        description: 'Pick the proper Age',
+        options: [
+          {value: 1, label: 'Modern'},
+          {value: 2, label: 'Copper'},
+          {value: 3, label: 'Bronze'},
+          {value: 4, label: 'Silver'},
+          {value: 5, label: 'Gold'},
+        ]
+      }
+    },
+  ];
+
+  onSubmit() {
+    console.log(this.model);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyMaterialModule } from '@ngx-formly/material';
+import { FormlyMatDatepickerModule } from '@ngx-formly/material/datepicker';
+import { FormlyMatToggleModule } from '@ngx-formly/material/toggle';
+
+import { MatDatepickerModule } from '@angular/material/datepicker'
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatRadioModule } from '@angular/material/radio'
+import { MatSelectModule } from '@angular/material/select'
+import { MatButtonModule } from '@angular/material/button'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { MatNativeDateModule } from '@angular/material/core'
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +27,23 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+
+    MatNativeDateModule,
+    FormlyMatDatepickerModule,
+    FormlyMatToggleModule,
+    FormlyModule.forRoot(),
+    FormlyMaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
